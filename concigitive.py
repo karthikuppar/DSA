93fe1bf8e1720ceb4bd06a456c280ffa1558216e
karthikuppar <karthikuppar463@gmail.com>
2026-05-03 23:17:01 +0530

    Add files via upload

---
+++ concigitive.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums):
+        cnt=0
+        maxi=0
+        for i in range(len(nums)):
+            if nums[i]==1:
+                cnt+=1
+            else:
+                 cnt=0
+            maxi=max(maxi,cnt)
+        return maxi
+
+a=Solution()
+nums=[1, 1, 0, 0, 1, 1, 1, 0]
+print(a.findMaxConsecutiveOnes(nums))
+        
