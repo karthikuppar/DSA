3cd6e4d465a1b02527b67337800f4e1b71c8424b
karthikuppar <karthikuppar463@gmail.com>
2026-08-25 23:33:48 +0530

    Add files via upload

---
+++ findtagetbs.py
@@ -0,0 +1,38 @@
+class Solution:
+    def search(self, nums, target):
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            # Left half is sorted
+            if nums[left] <= nums[mid]:
+
+                # Is target inside the sorted left half?
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+            # Right half is sorted
+            else:
+
+                # Is target inside the sorted right half?
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+        return -1
+
+
+a = Solution()
+
+nums = [3, 4, 5, 6, 1, 2]
+target = 1
+
+print(a.search(nums, target))
